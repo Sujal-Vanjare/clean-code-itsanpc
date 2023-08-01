f5cbad15ca5f46a245e2483ceb111401824963df
Sujal-Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2023-08-01 22:25:04 +0530

    theme changes, added focus outline

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Arrow from "@/components/HalfArrow/arrow";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -42,72 +43,158 @@ export default function Home() {
         <section className={styles.section}>
           <div className={styles.sectionContent}>
             <div className={styles.cardList}>
-              <div className={`${styles.card} ${styles.cardHero}`}>
-                <Image
-                  className={styles.heroImg}
-                  src="/16-9-img.png"
-                  width={640}
-                  height={360}
-                  alt="image of the card"
-                />
-                <div className={styles.cardDesc}>
-                  <div className={styles.cardHead}>
-                    <p className={styles.category}>VIDEO</p>
-                    <h2 className={styles.heroHeadline}>
-                      Principles of spatial design
-                    </h2>
-                    <p className={styles.paragraph}>
-                      Learn how to design with depth, scale, windows, and
-                      immersion, and apply best practices for creating
-                      comfortable, human-centered experiences that transform
-                      reality.
-                    </p>
+              <Link href="">
+                <div className={`${styles.card} ${styles.cardHero}`}>
+                  <Image
+                    className={styles.heroImg}
+                    src="/16-9-img.png"
+                    width={640}
+                    height={360}
+                    alt="image of the card"
+                  />
+                  <div className={styles.cardDesc}>
+                    <div className={styles.cardHead}>
+                      <p className={styles.category}>VIDEO</p>
+                      <h2 className={styles.heroHeadline}>
+                        Principles of spatial design
+                      </h2>
+                      <p className={styles.paragraph}>
+                        Learn how to design with depth, scale, windows, and
+                        immersion, and apply best practices for creating
+                        comfortable, human-centered experiences that transform
+                        reality.
+                      </p>
+                    </div>
+                    <div className={styles.cardDate}>Aug 01, 2023</div>
                   </div>
-                  <div className={styles.cardDate}>Aug 01, 2023</div>
                 </div>
-              </div>
-              <div className={`${styles.card} ${styles.card2up}`}>
-                <Image
-                  className={styles.heroImg}
-                  src="/16-9-img.png"
-                  width={640}
-                  height={360}
-                  alt="image of the card"
-                />
-                <div className={styles.cardDesc}>
-                  <div className={styles.cardHead}>
-                    <p className={styles.category}>VIDEO</p>
-                    <h2 className={styles.up2Headline}>
-                      Design dynamic Live Activities
-                    </h2>
-                    <p className={styles.paragraph}>
-                      Learn the best way to create graphically rich layouts that update seamlessly on the Lock Screen, in StandBy, and in the Dynamic Island.
-                    </p>
+              </Link>
+              <Link href="">
+                <div className={`${styles.card} ${styles.card2up}`}>
+                  <Image
+                    className={styles.heroImg}
+                    src="/16-9-img.png"
+                    width={640}
+                    height={360}
+                    alt="image of the card"
+                  />
+                  <div className={styles.cardDesc}>
+                    <div className={styles.cardHead}>
+                      <p className={styles.category}>VIDEO</p>
+                      <h2 className={styles.up2Headline}>
+                        Design dynamic Live Activities
+                      </h2>
+                      <p className={styles.paragraph}>
+                        Learn the best way to create graphically rich layouts that update seamlessly on the Lock Screen, in StandBy, and in the Dynamic Island.
+                      </p>
+                    </div>
+                    <div className={styles.cardDate}>Aug 01, 2023</div>
                   </div>
-                  <div className={styles.cardDate}>Aug 01, 2023</div>
                 </div>
-              </div>
-              <div className={`${styles.card} ${styles.card2up}`}>
-                <Image
-                  className={styles.heroImg}
-                  src="/16-9-img.png"
-                  width={640}
-                  height={360}
-                  alt="image of the card"
-                />
-                <div className={styles.cardDesc}>
-                  <div className={styles.cardHead}>
-                    <p className={styles.category}>VIDEO</p>
-                    <h2 className={styles.up2Headline}>
-                      Design dynamic Live Activities
-                    </h2>
-                    <p className={styles.paragraph}>
-                      Learn the best way to create graphically rich layouts that update seamlessly on the Lock Screen, in StandBy, and in the Dynamic Island.
-                    </p>
+              </Link>
+              <Link href="">
+                <div className={`${styles.card} ${styles.card2up}`}>
+                  <Image
+                    className={styles.heroImg}
+                    src="/16-9-img.png"
+                    width={640}
+                    height={360}
+                    alt="image of the card"
+                  />
+                  <div className={styles.cardDesc}>
+                    <div className={styles.cardHead}>
+                      <p className={styles.category}>VIDEO</p>
+                      <h2 className={styles.up2Headline}>
+                        Design dynamic Live Activities
+                      </h2>
+                      <p className={styles.paragraph}>
+                        Learn the best way to create graphically rich layouts that update seamlessly on the Lock Screen, in StandBy, and in the Dynamic Island.
+                      </p>
+                    </div>
+                    <div className={styles.cardDate}>Aug 01, 2023</div>
                   </div>
-                  <div className={styles.cardDate}>Aug 01, 2023</div>
                 </div>
-              </div>
+              </Link>
+
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.section}>
+          <div className={styles.sectionContent}>
+            <div className={styles.cardList}>
+              <Link href="">
+                <div className={`${styles.card} ${styles.cardHero}`}>
+                  <Image
+                    className={styles.heroImg}
+                    src="/16-9-img.png"
+                    width={640}
+                    height={360}
+                    alt="image of the card"
+                  />
+                  <div className={styles.cardDesc}>
+                    <div className={styles.cardHead}>
+                      <p className={styles.category}>VIDEO</p>
+                      <h2 className={styles.heroHeadline}>
+                        Principles of spatial design
+                      </h2>
+                      <p className={styles.paragraph}>
+                        Learn how to design with depth, scale, windows, and
+                        immersion, and apply best practices for creating
+                        comfortable, human-centered experiences that transform
+                        reality.
+                      </p>
+                    </div>
+                    <div className={styles.cardDate}>Aug 01, 2023</div>
+                  </div>
+                </div>
+              </Link>
+              <Link href="">
+                <div className={`${styles.card} ${styles.card2up}`}>
+                  <Image
+                    className={styles.heroImg}
+                    src="/16-9-img.png"
+                    width={640}
+                    height={360}
+                    alt="image of the card"
+                  />
+                  <div className={styles.cardDesc}>
+                    <div className={styles.cardHead}>
+                      <p className={styles.category}>VIDEO</p>
+                      <h2 className={styles.up2Headline}>
+                        Design dynamic Live Activities
+                      </h2>
+                      <p className={styles.paragraph}>
+                        Learn the best way to create graphically rich layouts that update seamlessly on the Lock Screen, in StandBy, and in the Dynamic Island.
+                      </p>
+                    </div>
+                    <div className={styles.cardDate}>Aug 01, 2023</div>
+                  </div>
+                </div>
+              </Link>
+              <Link href="">
+                <div className={`${styles.card} ${styles.card2up}`}>
+                  <Image
+                    className={styles.heroImg}
+                    src="/16-9-img.png"
+                    width={640}
+                    height={360}
+                    alt="image of the card"
+                  />
+                  <div className={styles.cardDesc}>
+                    <div className={styles.cardHead}>
+                      <p className={styles.category}>VIDEO</p>
+                      <h2 className={styles.up2Headline}>
+                        Design dynamic Live Activities
+                      </h2>
+                      <p className={styles.paragraph}>
+                        Learn the best way to create graphically rich layouts that update seamlessly on the Lock Screen, in StandBy, and in the Dynamic Island.
+                      </p>
+                    </div>
+                    <div className={styles.cardDate}>Aug 01, 2023</div>
+                  </div>
+                </div>
+              </Link>
 
             </div>
           </div>
