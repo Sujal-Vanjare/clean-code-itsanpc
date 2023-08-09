3e492c6e59355e4b429163f92a282b694f000883
Sujal-Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2023-08-09 23:39:20 +0530

    content

--- src/app/ssd/page.jsx
+++ src/app/ssd/page.jsx
@@ -3,7 +3,7 @@ import styles from "./page.module.css";
 export default function Ssd() {
     return (
         <>
-            <div>
+            <div className={styles.page}>
                 ssd page
             </div>
         </>
