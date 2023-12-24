10125a12bc662295b92bf37296bde255bd6d5feb
Sujal-Vanjare <141014768+Sujal-Vanjare@users.noreply.github.com>
2023-12-24 19:35:27 +0530

    Update urls.js

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,4 @@
 export const STRAPI_API_TOKEN = process.env.NEXT_PUBLIC_STRAPI_API_TOKEN;
 
-export const API_URL = "http://127.0.0.1:1337";
-
-// export const API_URL = process.env.NEXT_PUBLIC_API_URL;
+export const API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL || "http://localhost:1337";
